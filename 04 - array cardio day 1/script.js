9050f7986c89d40357182ed9a9833d3db9d91f40
Büşra <43908111+busracagliyan@users.noreply.github.com>
2022-04-14 12:04:53 +0300

    Create script.js

---
+++ 04 - array cardio day 1/script.js
@@ -0,0 +1,69 @@
+ const inventors = [
+   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+   { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+   { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+   { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+   { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+   { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+   { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+   { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+   { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+   { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+   { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+ ];
+
+const people = [
+  'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+  'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+  'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+  'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+  'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+];
+
+const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
+console.table(fifteen); 
+
+const fullnames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullnames);
+
+const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
+console.table(ordered);
+        
+const total_years = inventors.reduce((total,inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(total_years);
+
+const oldest = inventors.sort(function(a,b){
+  const last_guy = a.passed - a.year;
+  const first_guy = b.passed - b.year;
+  return last_guy > first_guy ? 1 : -1;
+});
+console.table(oldest)
+
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links
+  //.map(link => link.textContent)
+  //.filter(street_name => street_name.includes('de'));
+
+const alpha = people.sort(function(last_one, next_one) {
+  const [a_last, a_first] = last_one.split(', ');
+  const [b_last, b_first] =next_one.split(', ');
+  return a_last > b_last ? 1 : -1;
+});
+console.log(alpha);
+         
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick' ];
+
+const transportation = data.reduce(function(obj, item) {
+  if (!obj[item]){
+    obj[item] = 0
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(transportation);
